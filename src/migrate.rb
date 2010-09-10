c88d616bf65d59eb22a4a2a135f9ca5780035799
jbeck <justinbeck@mac.com>
2010-09-10 14:52:30 -0400

    Code cleanup.

--- src/migrate.rb
+++ src/migrate.rb
@@ -1,42 +1,32 @@
 require 'rubygems'
 
 require 'rails'
-
 require 'sunspot'
 require 'sunspot/rails'
-
 require 'active_record'
 require 'mysql'
 require 'logger'
-
 require 'models/parent'
 require 'models/child'
 
 logger = Logger.new(STDERR)
-
 ActiveRecord::Base.logger = logger
 ActiveRecord::Base.colorize_logging = true
 ActiveRecord::Base.establish_connection(:adapter => 'mysql', :host => 'localhost', :database => 'sunspot_test')
-        
 Sunspot::Adapters::InstanceAdapter.register(Sunspot::Rails::Adapters::ActiveRecordInstanceAdapter, ActiveRecord::Base)
 Sunspot::Adapters::DataAccessor.register(Sunspot::Rails::Adapters::ActiveRecordDataAccessor, ActiveRecord::Base)
 
 parent = Parent.create( :name => 'Big Jim')
-
 Child.create( :name => 'Bob', :parent => parent )
 Child.create( :name => 'Bill', :parent => parent )
 
 Sunspot.commit
 
 logger.debug "Defining Search"
-search = Child.search do
-  keywords ['bill']
-end
+search = Child.search { keywords ['bill'] }
 
 logger.debug "Hits: #{search.hits.size}"
-search.each_hit_with_result do |hit, child|
-  logger.debug child.name
-end
+search.each_hit_with_result { |hit, child| logger.debug child.name }
 
 Child.destroy_all
 Parent.destroy_all
